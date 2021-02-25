db8ee0eb25a1a0b38e453e8506cc16d59c19630f
Luan da Silva Ramalho <70976199+luanlsr@users.noreply.github.com>
2021-02-25 19:41:45 -0300

    Feito exercícios do dia 25/02

---
+++ BLOCO_4/exercises2/arrays.js
@@ -0,0 +1,56 @@
+// Exercício 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1){
+  console.log(numbers[index])
+}
+
+// Exercício 2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1){
+  soma = soma + numbers[index]
+}
+console.log(`O resultado da soma dos valores ${numbers} é: ${soma}`)
+
+//Exercício 3
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1){
+  soma = soma + numbers[index]
+}
+
+let media = soma/numbers.length
+
+console.log(`O resultado da média dos valores ${numbers} é: ${media}`)
+
+// Exercício 4
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1){
+  soma = soma + numbers[index]
+}
+
+let media = soma/numbers.length
+
+console.log(`O resultado da média dos valores ${numbers} é: ${media}`)
+
+if(media >= 20){
+  console.log('Valor maior que 20');
+
+}else{
+  console.log('Valor menor que 20')
+}
+
+// Exercício 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1){
+  soma = soma + numbers[index]
+}
+
+// Exercício 6
