a14cf8007d3dd8e76766e8538cf8bec58ffdbb66
Luan da Silva Ramalho <70976199+luanlsr@users.noreply.github.com>
2021-02-26 15:34:24 -0300

    Terminado exercício 6 maior valor

--- BLOCO_4/exercises2/arrays.js
+++ BLOCO_4/exercises2/arrays.js
@@ -47,10 +47,15 @@ if(media >= 20){
 
 // Exercício 5
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = 0
 
-let soma = 0;
 for (let index = 0; index < numbers.length; index += 1){
-  soma = soma + numbers[index]
+  let eachNumber = numbers[index];
+  
+    if(eachNumber > higherNumber){
+      higherNumber = eachNumber
+    }      
 }
 
+console.log(higherNumber)
 // Exercício 6
