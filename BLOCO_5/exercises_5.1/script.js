3c9f0f008506929851a686874bb504b7f6577422
luanlsr <luanfswd@gmail.com>
2021-03-03 23:34:39 -0300

    Criado base para os exercícios

--- BLOCO_5/exercises_5.1/script.js
+++
@@ -1,9 +0,0 @@
-let paragraph = document.getElementById("paragraph");
-      paragraph.style.color = "red";
-      
-      function subtitle() {
-      let sub = document.getElementById("subtitle");
-      sub.style.backgroundColor= "red";
-      sub.style.width = "200px";
-      sub.style.textAlign = "center";
-      }
