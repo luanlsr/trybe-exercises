d032c614658f390826dc1eb939a9b8c8a9fdb2af
luanlsr <luanfswd@gmail.com>
2021-04-27 12:07:40 -0300

    BLOCO 11

---
+++ BLOCO_11/exercise_11-1/hello-world-aula-11-1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Primeiro from './components/Titulo'
+
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Primeiro></Primeiro>
+      <h1>Olá, mundo!</h1>
+    </React.Fragment>
+  )
+}
+
+export default App;
---
+++ BLOCO_11/exercise_11-1/hello-world-aula-11-1/src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Primeiro = () => {
+  return (
+    <h1>Primeiro componente em React</h1>
+  );
+}
+
+export default Primeiro
