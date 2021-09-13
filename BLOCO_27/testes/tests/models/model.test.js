8c00804b2750572c82827bb6aae51ff74b3e675c
luanlsr <luanfswd@gmail.com>
2021-09-13 16:32:53 -0300

    bloco27

---
+++ BLOCO_27/testes/tests/models/model.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+
+// const fs = require('fs');
+// const sinon = require('sinon');
+
+// sinon.stub(fs, 'readFileSync')
+//   .returns('Valor a ser retornado');
+
+describe('A API deverá permitir a inserção de filmes no banco de dados', () => {
+  it('Ela deve receber e registrar as seguintes informações do filme: Nome , Direção e Ano de lançamento', () => {
+    const {name, direction, releaseYear} = 
+    //
+    expect(resposta).to.be.equals('reprovado');
+  });
+  it('Ao realizar a inserção de um novo filme, o endpoint deverá responder com o respectivo ID', () => {
+    //
+    expect(resposta).to.be.equals('reprovado');
+  });
+});
